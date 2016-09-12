b9170f07a284093675b9658c6829fe847c71f159
evanzummeren <erikvanzummeren@gmail.com>
2016-09-12 11:49:50 +0200

    werkende scraper

--- scraper_literatuur.js
+++ scraper_literatuur.js
@@ -1,12 +1,14 @@
+var he = require('he');
+
 var Xray = require('x-ray');
 var x = Xray();
 
 var author = process.argv[2];
 var firstPage = process.argv[3];
 var amountPages = process.argv[4];
-amountPages++;
 
 var scrapedUrl = 'http://www.dbnl.org/tekst/' + author + '_01/' + author + '_01_00';
+var he = require('he');
 
 var i = firstPage;
 
@@ -21,22 +23,29 @@ function scraperTimeLoop () {
 				var currentNumber = i;
 			}
 
-			x(scrapedUrl + currentNumber + '.php', '#tekst_en_noten', [{
-		  text: 'h2',
+			x(scrapedUrl + currentNumber + '.php', 'article', [{
+		  text: '#tekst_en_noten@html',
 			}])(function(err, obj) {
 
-				console.log(obj[0].h2);
+				var dirtyText = obj[0].text;
+				/* Remove everything inside <a href> */
+				dirtyText = dirtyText.replace(/<a.*a>/g, '');
+				/* Remove toneel lines */
+				dirtyText = dirtyText.replace(/<div class="line-nr.*div>/g, '');
+				/* Remove notes */
+				dirtyText = dirtyText.replace(/<div class="notes(.|\s)*<\/div>/g, '');
 
-			// console.log(obj[0].heading);
-			// console.log('current page ' + currentNumber);
-			// text += obj[0].heading;
-			// console.log(text);
+				/* Remove brackets with page numbers */
+				dirtyText = dirtyText.replace(/\[.*?\]/g,' ');
+				/* Remove all HTML junk */
+				dirtyText = dirtyText.replace(/<\/?[^>]+(>|$)/g, "");
 
+				/* Decode entity and RM multilines */
+				var cleanText = he.decode(dirtyText);
+				cleanText = cleanText.replace(/\n\s*\n\s*\n/g, "\n\n");
 
-			for (a = 0; a < obj.length; a++) { 
-				// text += ' ' + obj[i].text;
-					console.log(obj[a].text);
-				}
+				console.log(cleanText);
+
 			})
 
       i++;                    
